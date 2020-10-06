0137f489d89f0831d3ad97927743ad465c1e0984
rkynn <rakeyannuramria@gmail.com>
2020-10-06 13:57:26 +0700

    + Materi Views

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non quaerat, quibusdam placeat, repudiandae est nulla inventore at quasi molestiae atque exercitationem quos iusto eligendi ratione quod error fuga ad eaque.</p>
+        </div>
+    </div>
+</div>
